e395c0979baec69ff01849c2df9153b47c2a7589
Matthew Teferi <metthewsteferi@gmail.com>
2026-02-14 05:09:53 -0800

    Add projects API endpoints

--- fairness_tracker/urls.py
+++ fairness_tracker/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/', include('teams.urls')),
+    path('api/', include('projects.urls')),
 ]
 
 if settings.DEBUG:
