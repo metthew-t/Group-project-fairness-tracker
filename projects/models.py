cc441eeffc98689659593ac2c153b94d1ca18d37
Matthew Teferi <metthewsteferi@gmail.com>
2026-02-14 05:18:34 -0800

    Add projects app with CRUD and permissions

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import User
+from accounts.models import CustomUser
 from teams.models import Team
 
 class Project(models.Model):
@@ -12,7 +12,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='projects')
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_projects')
+    created_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, related_name='created_projects')
     deadline = models.DateField(null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PLANNING')
     created_at = models.DateTimeField(auto_now_add=True)
